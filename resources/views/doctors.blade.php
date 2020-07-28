37255397347ec7682a5b2cc7517e0f388237535c
John <johndoe@example.com>
2020-07-28 12:17:44 +0000

    page doctor

--- resources/views/doctors.blade.php
+++ resources/views/doctors.blade.php
@@ -1,5 +1,129 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>DOCTORS</h1>
+
+<div class="page-banner">
+	<div class="bg-page"></div>
+	<div class="text">
+		<h1>Doctors</h1>
+		<nav aria-label="breadcrumb">
+		  	<ol class="breadcrumb justify-content-center">
+			    <li class="breadcrumb-item"><a href="home">Home</a></li>
+			    <li class="breadcrumb-item active" aria-current="page">Doctors</li>
+		  	</ol>
+		</nav>
+	</div>
+</div>
+
+<div class="page-content">
+	<div class="container">
+		<div class="row team pt_0 pb_40">
+			<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
+				<div class="team-item">
+					<div class="team-photo">
+						<img src="img/doctor-1.jpg" alt="Doctor Photo">
+					</div>
+					<div class="team-text">
+						<h4>Dr. Brent Grundy</h4>
+						<h4>Chairman, AA Hospital</h4>
+
+						<div class="team-social">
+							<ul>
+								<li><a href="https://fr-fr.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://fr.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube"></i></a></li>
+								<li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="page-content">
+	<div class="container">
+		<div class="row team pt_0 pb_40">
+			<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
+				<div class="team-item">
+					<div class="team-photo">
+						<img src="img/doctor-2.jpg" alt="Doctor Photo">
+					</div>
+					<div class="team-text">
+						<h4>Dr. Robin Cook</h4>
+						<h4>Medecine, BB Hospital</h4>
+
+						<div class="team-social">
+							<ul>
+								<li><a href="https://fr-fr.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://fr.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube"></i></a></li>
+								<li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="page-content">
+	<div class="container">
+		<div class="row team pt_0 pb_40">
+			<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
+				<div class="team-item">
+					<div class="team-photo">
+						<img src="img/doctor-3.jpg" alt="Doctor Photo">
+					</div>
+					<div class="team-text">
+						<h4>Dr. Bob Smith</h4>
+						<h4>Neurologist, CC Clinic</h4>
+
+						<div class="team-social">
+							<ul>
+								<li><a href="https://fr-fr.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://fr.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube"></i></a></li>
+								<li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="page-content">
+	<div class="container">
+		<div class="row team pt_0 pb_40">
+			<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
+				<div class="team-item">
+					<div class="team-photo">
+						<img src="img/doctor-4.jpg" alt="Doctor Photo">
+					</div>
+					<div class="team-text">
+						<h4>Dr. Patrick Henderson</h4>
+						<h4>Cardiologist, DD Clinic</h4>
+
+						<div class="team-social">
+							<ul>
+								<li><a href="https://fr-fr.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://fr.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube"></i></a></li>
+								<li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
