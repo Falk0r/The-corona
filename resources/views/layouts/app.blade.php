70638cc9c232f70c0851bf3a31e6a035811cef81
olivier-sonrel <o.sonrel@it-students.fr>
2020-07-28 12:05:51 +0300

    all models done, yeeee i'm a fu;:,:, robot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/3c7f5856f6.js" crossorigin="anonymous"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
